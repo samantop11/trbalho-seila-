24bcec8c39bc7174a8b8061514350f90750dd622
samantop11 <samantha.santos.alves@escola.pr.gov.br>
2025-09-02 12:27:48 -0300

    Add files via upload

--- samantha 1d/conexao.php
+++ samantha 1d/conexao.php
@@ -4,7 +4,7 @@
 $host    = "localhost";   
 $usuario = "root";       
 $senha   = "";          
-$banco   = "";     
+$banco   = "sa213";     
 
 $conexao = mysqli_connect($host, $usuario, $senha, $banco);
 
@@ -17,12 +17,13 @@ mysqli_set_charset($conexao, "utf8");
 <!DOCTYPE html>
 <html lang="en">
 <head>
+<link rel="stylesheet" href="style.css"> 
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>login</title>
 </head>
 <body>
-    <h3>eiakaka</h3>
+    <h3></h3>
     
 </body>
 </html>
