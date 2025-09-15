52d451c48dc3fd760a0c686657e21966245fb02b
samantop11 <samantha.santos.alves@escola.pr.gov.br>
2025-09-15 11:18:02 -0300

    Add files via upload
    apenas html index.php

--- samantha 1d/conexao.php
+++ samantha 1d/conexao.php
@@ -14,16 +14,3 @@ if (!$conexao) {
 
 mysqli_set_charset($conexao, "utf8");
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-<link rel="stylesheet" href="style.css"> 
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>login</title>
-</head>
-<body>
-    <h3></h3>
-    
-</body>
-</html>
