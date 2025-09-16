265ecbfaf4f2704d5465d0860eae4b1d079d3801
samantop11 <samantha.santos.alves@escola.pr.gov.br>
2025-09-16 12:16:40 -0300

    Add files via upload

--- samantha 1d/page.php
+++ samantha 1d/page.php
@@ -1,12 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <link rel="stylesheet" href="style.css" type="text/css">
+ <link rel="stylesheet" href="style.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
 <body>
-    
+    <h2>FORMULARIO</h2>
+<div class="seila">
+    <form>
+     <input type="text"placeholder="login"id="login">
+     <input type="passaword" placeholder="senha" id="senha">
+     <input type="tel"placeholder="telefone" id="telefone">
+     <input type="submit"onclick="logar(); return false;">
+</form>
+</div>
 </body>
 </html>
