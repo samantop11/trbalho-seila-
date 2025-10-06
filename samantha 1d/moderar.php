308079fbb107c77a38bccbea5329f23e68427676
samantop11 <samantha.santos.alves@escola.pr.gov.br>
2025-10-06 11:30:59 -0300

    Add files via upload

--- samantha 1d/moderar.php
+++ samantha 1d/moderar.php
@@ -1,5 +1,7 @@
 <?php
 include "conexao.php";
+
+// Atualizar recado
 if(isset($_POST['atualiza'])){
     $idatualiza = intval($_POST['id']);
     $nome       = mysqli_real_escape_string($conexao, $_POST['nome']);
@@ -10,17 +12,29 @@ if(isset($_POST['atualiza'])){
     mysqli_query($conexao, $sql) or die("Erro ao atualizar: " . mysqli_error($conexao));
     header("Location: moderar.php");
     exit;
-}if(isset($_GET['acao']) && $_GET['acao'] == 'excluir'){
+}
+
+// Excluir recado
+if(isset($_GET['acao']) && $_GET['acao'] == 'excluir'){
     $id = intval($_GET['id']);
     mysqli_query($conexao, "DELETE FROM samantha WHERE id=$id") or die("Erro ao deletar: " . mysqli_error($conexao));
     header("Location: moderar.php");
     exit;
 }
+
+// Editar recado
+$editar_id = isset($_GET['acao']) && $_GET['acao'] == 'editar' ? intval($_GET['id']) : 0;
+$recado_editar = null;
+if($editar_id){
+    $res = mysqli_query($conexao, "SELECT * FROM samantha WHERE id=$editar_id");
+    $recado_editar = mysqli_fetch_assoc($res);
+}
+?>
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
 <meta charset="utf-8"/>
-<title>MoSderar pedidos</title>
+<title>Moderar pedidos</title>
 <link rel="stylesheet" href="style.css"/>
 </head>
 <body>
@@ -29,29 +43,31 @@ if(isset($_POST['atualiza'])){
 <div id="header">
     <h1>Mural de pedidos</h1>
 </div>
+
 <?php if($recado_editar): ?>
-    <div id="formulario_mural">
-    <form method="post">
-        <label>Nome:</label>
-        <input type="text" name="nome" value="<?php echo htmlspecialchars($recado_editar['nome']); ?>"/><br/>
-        <label>Email:</label>
-        <input type="text" name="email" value="<?php echo htmlspecialchars($recado_editar['email']); ?>"/><br/>
-        <label>Mensagem:</label>
-        <textarea name="msg"><?php echo htmlspecialchars($recado_editar['mensagem']); ?></textarea><br/>
-        <input type="hidden" name="id" value="<?php echo $recado_editar['id']; ?>"/>
-        <input type="submit" name="atualiza" value="Modificar Recado" class="btn"/>
-    </form>
-    </div>
-    <?php endif; ?>
-    <?php
+<div id="formulario_mural">
+<form method="post">
+    <label>Nome:</label>
+    <input type="text" name="nome" value="<?php echo htmlspecialchars($recado_editar['nome']); ?>"/><br/>
+    <label>Email:</label>
+    <input type="text" name="email" value="<?php echo htmlspecialchars($recado_editar['email']); ?>"/><br/>
+    <label>Mensagem:</label>
+    <textarea name="msg"><?php echo htmlspecialchars($recado_editar['mensagem']); ?></textarea><br/>
+    <input type="hidden" name="id" value="<?php echo $recado_editar['id']; ?>"/>
+    <input type="submit" name="atualiza" value="Modificar samantha" class="btn"/>
+</form>
+</div>
+<?php endif; ?>
+
+<?php
 $seleciona = mysqli_query($conexao, "SELECT * FROM samantha ORDER BY id DESC");
 if(mysqli_num_rows($seleciona) <= 0){
     echo "<p>Nenhum pedido no mural!</p>";
 }else{
     while($res = mysqli_fetch_assoc($seleciona)){
         echo '<ul class="samantha">';
-        echo '<li><strong>ID:</strong> ' . $res['id'] . ' |
-              <a href="moderar.php?acao=excluir&id=' . $res['id'] . '">Remover</a> |
+        echo '<li><strong>ID:</strong> ' . $res['id'] . ' | 
+              <a href="moderar.php?acao=excluir&id=' . $res['id'] . '">Remover</a> | 
               <a href="moderar.php?acao=editar&id=' . $res['id'] . '">Modificar</a></li>';
         echo '<li><strong>Nome:</strong> ' . htmlspecialchars($res['nome']) . '</li>';
         echo '<li><strong>Email:</strong> ' . htmlspecialchars($res['email']) . '</li>';
@@ -60,6 +76,7 @@ if(mysqli_num_rows($seleciona) <= 0){
     }
 }
 ?>
+
 <div id="footer">
 </div>
 </div>
